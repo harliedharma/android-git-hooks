04220cf6c5731a6cc1032e5a99213a6c58c226da
harlie dharma <t-harlie.dharma@traveloka.com>
2018-04-10 17:08:09 +0700

    Copy lint result to project build folders

--- android-linter.py
+++ android-linter.py
@@ -2,6 +2,7 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 import subprocess
+import shutil
 
 PRODUCT_FLAVOR = "GeneralStaging"
 PROJECT_ROOT = None
@@ -33,29 +34,35 @@ def main():
                 continue
             modules[folders[0]] = 1
 
+
+    lint_results_dir = PROJECT_ROOT + "/build/lint"
+    if os.path.exists(lint_results_dir):
+        shutil.rmtree(lint_results_dir)
+    os.makedirs(lint_results_dir)
+
     issues = {}
     for module in modules:
         issuesModule = issues[module] = {}
         gradlew = PROJECT_ROOT + "/gradlew"
-        report = PROJECT_ROOT + "/" + module + "/build/reports/lint-results" + (("-" + PRODUCT_FLAVOR[0].lower() + PRODUCT_FLAVOR[1:] ) if PRODUCT_FLAVOR is not None else "" ) +  ".xml"
+        report_dir = PROJECT_ROOT + "/" + module + "/build/reports"
+        report_filename = "lint-results" + (("-" + PRODUCT_FLAVOR[0].lower() + PRODUCT_FLAVOR[1:] ) if PRODUCT_FLAVOR is not None else "")
+        report_xml = report_dir + "/" + report_filename + ".xml"
+        report_html = report_dir + "/" + report_filename + ".html"
         # if os.path.exists(report):
         #     os.remove(report)
         cmd = gradlew + " " + module + ":lint" + PRODUCT_FLAVOR
         print(cmd)
-        # os.system(cmd)
-        # os.WEXITSTATUS(os.system(cmd))
         process = subprocess.Popen([gradlew, module + ":lint" + PRODUCT_FLAVOR])
         process.wait()
         if process.returncode != 0:
             print("Error: Lint failed on module " + module)
             sys.exit(1)
         
-        print("Parsing: " + report)
-
-        if not os.path.exists(report):
+        print("Parsing: " + report_xml)
+        if not os.path.exists(report_xml):
             print("Error: Lint result not found on module " + module)
             sys.exit(1)
-        xml = ET.parse(report)
+        xml = ET.parse(report_xml)
         root = xml.getroot()
         for issue in root:
             severity = issue.attrib['severity']
@@ -63,6 +70,8 @@ def main():
                 issuesModule[severity] = 1
             else:
                 issuesModule[severity] += 1
+        shutil.copyfile(report_xml, lint_results_dir + "/" + module + "-" + report_filename + ".xml")
+        shutil.copyfile(report_html, lint_results_dir + "/" + module + "-" + report_filename + ".html")
 
     print("Lint Result:")
     if len(issues) == 0:
